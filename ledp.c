639ab24d1d1e3f49a6c5c1277de388fdb6033b68
urrmina <drdramina1st@gmail.com>
2022-05-16 19:44:13 +0100

    first commit

--- ledp.c
+++ ledp.c
@@ -1,30 +1,19 @@
-#include <stdio.h>
-#include "contiki.h"
-#include "dev/leds.h"
-
-/* test with skymote LEDs*/
-
-/*Creating a timer, etimer is event time*/
-static struct etimer blinktimer;
-
-static uint8_t blinks; /*a date type that have 8bit variables*/
-
-PROCESS(blink_process, "LED BLINK PROCESS"); /*a contiki process */
-AUTOSTART_PROCESSES(&blink_process);
- 
- PROCESS_THREAD(blink_process,ev,data) /*ev is event time*/
+ #include "contiki.h"
+ #include <stdio.h>
+ #include "dev/leds.h"
+ #include "dev/button-sensor.h"
+ PROCESS(led_process,"LED_PROCESS");
+ AUTOSTART_PROCESSES(&led_process);
+ PROCESS_THREAD(led_process,ev,data)
  {
  PROCESS_BEGIN();
- blinks=0;
+ SENSORS_ACTIVATE(button_sensor);
  while(1)
  {
- etimer_set(&blinktimer, CLOCK_SECOND);
- /* CLOCK_SECOND generates 1 to 10 ms timing signals according to the motes */
- PROCESS_WAIT_EVENT_UNTIL(ev==PROCESS_EVENT_TIMER);
- leds_off(LEDS_ALL);
- leds_on(blinks & LEDS_ALL);
- blinks++;
- printf("State of LED %02X \n", leds_get());
+ printf("Press the button of the sensor");
+ PROCESS_WAIT_EVENT_UNTIL(ev=sensors_event && data==&button_sensor);
+ leds_toggle(LEDS_GREEN);
  }
+ printf("The sensor valus ls %u \n", leds_get());
  PROCESS_END();
  }
