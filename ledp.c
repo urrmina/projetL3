2d5e7217ad76d9524a59d292ed1d06fd46df53e3
urrmina <drdramina1st@gmail.com>
2022-07-02 00:14:51 +0100

    new version changes

--- ledp.c
+++ ledp.c
@@ -12,10 +12,14 @@ PROCESS_THREAD(blink_timer_process, ev, data)
   PROCESS_BEGIN();
  
   /* Initializing stuff here */ 
-	SENSORS_ACTIVATE(button_sensor);
+	SENSORS_ACTIVATE(button_sensor); // activate the button sensor
 	leds_off(LEDS_ALL);
 	printf("All leds are off\n");   
 	printf("Press the user button to start\n");
+	/* For the lights to function */
+	leds_toggle(LEDS_RED); //keeps lights red when they turn
+	leds_off(LEDS_ALL);
+
  
     while(1) {
 	static uint32_t ticks = 0;
@@ -24,25 +28,37 @@ PROCESS_THREAD(blink_timer_process, ev, data)
  
 	PROCESS_WAIT_EVENT();
  
-	if(ev == sensors_event) {  // If the event it's provoked by the user button, then...
-           if(data == &button_sensor) {		
+	if(ticks < 11550) { //the led will be on for approximatly five minute (1min = 2310ticks)
+		if (ev == sensors_event && data == &button_sensor) {	
 		etimer_set(&et, CLOCK_SECOND*seconds);  // Set the timer
-		printf("Timer started\n");
-           }
+		printf("#########\n######### Timer started #########\n#########\n");
         }
- 
-	if(etimer_expired(&et)) {  // If the event it's provoked by the timer expiration, then...
-		leds_toggle(LEDS_BLUE);
-		if (ticks % 2 == 0) {
-			printf("LED BLUE .............. [ON]\n");
-                }
-		else { 
-			printf("LED BLUE ............. [OFF]\n");
-                }
-		etimer_reset(&et);
-		ticks++;
-                }	
-	}		
+ 	
+		if(etimer_expired(&et)) {  // If the event it's provoked by the timer expiration, then...
+			
+			if (ticks % 2 == 0) {
+				printf("LED GREEN [ON]  LED RED [OFF]\n");
+				leds_on(LEDS_GREEN);
+				leds_off(LEDS_RED);
+					}
+			else {
+				printf("LED GREEN [OFF] LED RED [ON] \n");
+				leds_off(LEDS_GREEN);
+				leds_on(LEDS_RED);
+					}
+				
+			etimer_reset(&et);
+			ticks++;
+					}
+	} else {
+		leds_off(LEDS_RED);
+		leds_off(LEDS_GREEN);
+		ticks = 0;
+		printf("All leds are off\n");   
+		printf("Press the user button to start again\n");
+	}	
+	}
+		
 	exit:
 		leds_off(LEDS_ALL);
 		PROCESS_END();
